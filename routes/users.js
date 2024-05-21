950ee44bd5cde987bdc877edf00f97e0a5e57d91
soyeon <uyyu1734@gmail.com>
2024-05-21 10:24:55 +0900

    feat: login

--- routes/users.js
+++ routes/users.js
@@ -17,5 +17,23 @@ router.post("/signup", async (req, res, next) => {
     next(err);
   }
 });
-
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.login(email, password);
+    const tokenMaxAge = 60 * 60 * 24 * 3;
+    const token = createToken(user, tokenMaxAge);
+    user.token = token;
+    res.cookie("authToken", token, {
+      httpOnly: true,
+      maxAge: tokenMaxAge * 1000,
+    });
+    console.log(user);
+    res.status(201).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(400);
+    next(err);
+  }
+});
 module.exports = router;
