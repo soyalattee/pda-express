4f663c6f6b76b3aa22fbe453a6cb0e2039688fe8
soyeon <uyyu1734@gmail.com>
2024-05-21 09:17:53 +0900

    feat: bcrypt 추가하고 user스키마 작성

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { isEmail } = require("validator");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, " ."],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, " ."],
+  },
+  password: {
+    type: String,
+    required: [true, " ."],
+  },
+});
+userSchema.statics.signUp = async function (email, password) {
+  const salt = await bcrypt.genSalt();
+  console.log(salt);
+  try {
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const use = await this.create({ email, password: hashedPassword });
+    return {
+      _id: user._id,
+      email: user.email,
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user.visibleUser;
+    }
+    throw Error("incorrect password");
+  }
+  throw Error("incorrect email");
+};
+const visibleUser = userSchema.virtual("visibleUser");
+visibleUser.get(function (value, virtual, doc) {
+  return {
+    _id: doc._id,
+    email: doc.email,
+  };
+});
+const User = mongoose.model("user", userSchema);
+module.exports = User;
