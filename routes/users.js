68e69e1fc29eb9e5845ec3cb61734b62fda6b51a
soyeon <uyyu1734@gmail.com>
2024-05-21 11:00:22 +0900

    feat: jwt토큰 체크 로직

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require("express");
 const User = require("../models/User");
+const { createToken, verifyToken } = require("../utils/auth");
 var router = express.Router();
 
 /* GET users listing. */
@@ -36,4 +37,19 @@ router.post("/login", async (req, res, next) => {
     next(err);
   }
 });
+router.all("/logout", async (req, res, next) => {
+  try {
+    console.log(res.cookie);
+    res.cookie("authToken", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    });
+    res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    res.status(400);
+    next(err);
+  }
+});
+
 module.exports = router;
